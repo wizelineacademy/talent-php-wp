1ea87c55c328d546cdb4465bd05e51c84db7dafd
Jesús Fernando Alvarez Franco <jefer_590@hotmail.com>
2018-09-18 00:00:04 -0500

    :sparkles: - Challenge 2 Completed

---
+++ events/events.php
@@ -0,0 +1,13 @@
+<?php
+require_once "vendor/autoload.php";
+
+/*
+ * Plugin Name: Events Plugin
+ * Plugin URI: https://fernalvarez.com
+ * Description: Render a list of events
+ * Version: 1.0.0
+ * Author: Fernando Alvarez
+ */
+
+new EventsPlugin\Events();
+new EventsPlugin\EventsOptions();
